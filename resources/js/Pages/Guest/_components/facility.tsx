96d9ce369c15e6e11b8b1d94f1ab3a0afd02263f
Ardiman <pc.ardiman98@gmail.com>
2024-02-05 18:36:08 +0700

    update ui

--- resources/js/Pages/Guest/_components/facility.tsx
+++ resources/js/Pages/Guest/_components/facility.tsx
@@ -10,22 +10,37 @@ const numbers = Array.from({ length: 11 }, (_, i) => i + 1);
 
 const Facility = () => {
     return (
-        <div className="flex md:flex-row flex-col gap-y-3 md:gap-y-0">
-            <div className="md:w-1/2 w-full">
-                <h1 className="font-extrabold md:text-4xl text-2xl  mt-6 md:leading-snug ">
+        <div className="flex flex-col gap-y-3 md:gap-y-0">
+            <div className="w-full ">
+                <h1 className="font-extrabold md:text-4xl text-2xl  mt-6 md:leading-snug text-center">
                     Ayo, Bergabung bersama kami !
                 </h1>
-                <p className="mt-4 text-muted-foreground">
+                <p className="mt-4 text-muted-foreground text-center">
                     Berbagai fasilitas modern dan terkini tersedia di kampus
                     kami.
                 </p>
             </div>
-            <div className="md:w-1/2 w-full">
+            <br />
+            <div className="w-full mt-[24px]">
                 <Swiper
                     slidesPerView={"auto"}
                     spaceBetween={30}
                     pagination={{
                         clickable: true,
+                    }}
+                    breakpoints={{
+                        640: {
+                            slidesPerView: 2,
+                            spaceBetween: 20,
+                        },
+                        768: {
+                            slidesPerView: 3,
+                            spaceBetween: 40,
+                        },
+                        1024: {
+                            slidesPerView: 3,
+                            spaceBetween: 50,
+                        },
                     }}
                     modules={[Pagination, Autoplay]}
                     autoplay
